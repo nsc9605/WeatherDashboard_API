b98847be87be42e81ee4c9462a69353b0e51ff13
Niki Catapano <nsc9605@gmail.com>
2020-11-25 12:23:32 -0500

    added form and styling to html and css

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,9 @@
-// var APIKey = "ec69048cb2b0cadd73309ba3b0504619"
+// Set current time in header when application is opened
+$(document).ready(function () {
+    var today = moment();
+    $("#currentDay").text(today.format("dddd MMMM Do YYYY, h:mm a"));
 
-// var queryURL = "https://api.openweathermap.org/data/2.5/onecall?lat={40.7128}&lon={74.0060}&exclude={part}&appid={ec69048cb2b0cadd73309ba3b0504619}"
+
+var APIKey = "ec69048cb2b0cadd73309ba3b0504619"
+
+var queryURL = "https://api.openweathermap.org/data/2.5/onecall?lat=40.7128&lon=74.0060&exclude=part&appid=ec69048cb2b0cadd73309ba3b0504619"
