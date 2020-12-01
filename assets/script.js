b2a783420f0a880cf14664aa8a69a4d7d1d2ea5a
Niki Catapano <nsc9605@gmail.com>
2020-12-01 17:52:26 -0500

    added to javascript, css

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,24 @@
+
+let apiKey = "ec69048cb2b0cadd73309ba3b0504619";
+let searchBtn = $(".searchBtn");
+let searchInput = $(".searchInput");
+let cityStateEl = $(".city");
+let currentDateEl = $(".currentDate");
+
 // Set current time in header when application is opened
 $(document).ready(function () {
-    var today = moment();
-    $("#currentDay").text(today.format("dddd MMMM Do YYYY, h:mm a"));
+    let today = moment();
+    $("#currentDate").text(today.format("dddd MMMM Do YYYY, h:mm a"));
 
 
-var APIKey = "ec69048cb2b0cadd73309ba3b0504619"
+    var queryURL = "https://api.openweathermap.org/data/2.5/onecall?lat=40.7128&lon=74.0060&exclude=part&appid=ec69048cb2b0cadd73309ba3b0504619";
 
-var queryURL = "https://api.openweathermap.org/data/2.5/onecall?lat=40.7128&lon=74.0060&exclude=part&appid=ec69048cb2b0cadd73309ba3b0504619"
+    
+$.ajax({
+url: queryURL,
+method: "GET",
+})
+.then(function(response){
+    
+})
+
