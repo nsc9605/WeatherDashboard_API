692f3ee3fc3df1cc6522aaa7c5cb23ada9ab5d5e
Niki Catapano <nsc9605@gmail.com>
2021-01-05 15:11:13 -0500

    updated formating/style, added five-day weather info with icons

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,5 @@
 console.log("works");
 
-// Set current time in header when application is opened
-// $(document).ready(function () {
-//     let today = moment();
-// $("#currentDate").text(today.format("dddd MMMM Do YYYY, h:mm a"));
-
 // Global Variables
 let apiKey = "c6c3415f70d6fade0d7d3230231ef65e";
 let currentDate = moment().format("MM/DD/YYYY");
@@ -17,6 +12,7 @@ let windEl = "#wind";
 let uvIndexEl = "#uvIndex";
 let cityNameEl = "#cityName";
 let weatherDescription = "#weatherDescription";
+
 
 // Submit event
 document
@@ -33,17 +29,16 @@ function getWeather(cityName) {
   console.log(cityName);
   let queryURLForToday = `https://api.openweathermap.org/data/2.5/weather?units=imperial&q=${cityName}&appid=${apiKey}`;
   //   let queryURLForToday = `https://api.openweathermap.org/data/2.5/onecall?units=imperial&q=${cityName}&appid=${apiKey}`;
-
   fetch(queryURLForToday)
     .then(function (weatherResponse) {
       return weatherResponse.json();
     })
     .then(function (data) {
       console.log(data);
-      showWeatherForToday(data);
+      showWeatherForToday(cityName, data);
 
       // Call 5 day URL with lat lon from the one day response
-      let queryURLForFiveDay = `https://api.openweathermap.org/data/2.5/onecall?lat=${data.coord.lat}&lon=${data.coord.lon}&appid=${apiKey}`;
+      let queryURLForFiveDay = `https://api.openweathermap.org/data/2.5/onecall?units=imperial&lat=${data.coord.lat}&lon=${data.coord.lon}&appid=${apiKey}`;
       fetch(queryURLForFiveDay)
         .then(function (weatherResponse) {
           return weatherResponse.json();
@@ -56,46 +51,46 @@ function getWeather(cityName) {
 }
 
 // Show data on page
-function showWeatherForToday(data) {
-  document.querySelector("#currentCity").innerHTML = data.name;
+function showWeatherForToday(cityName, data) {
+ 
+  document.querySelector("#currentDate").innerHTML = moment().format("MMMM Do, YYYY, hh:mm");
+  document.querySelector("#weatherDescription").innerHTML = data.weather[0].description;
+  document.querySelector("#currentIcon").innerHTML = `<img src="http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png"`;
+  // document.querySelector("#cityImage").innerHTML = `<img src="http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+  document.querySelector("#currentCity").innerHTML = cityName;
   document.querySelector("#temp").innerHTML = data.main.temp + "°F";
   document.querySelector("#humidity").innerHTML = data.main.humidity + "%";
-  document.querySelector("#wind").innerHTML = data.wind + "mph";
-  document.querySelector("#uvIndex").innerHTML = data.main.uvIndex;
-  // finish the rest of today for html
-}
+  document.querySelector("#wind").innerHTML = data.wind.speed + "mph";
+  // document.querySelector("#uvIndex").innerHTML = data.main.uvi;
+ }
 
 // five day
 function showFiveDayWeather(data) {
+
+  // For loop to pull weather for 5 day forecast
   for (var i = 0; i < 5; i++) {
-
-    // let tempLow = Math.floor(data.daily[i].temp.min);
-    // let tempHigh = Math.floor(data.daily[i].temp.max);
-
+    // Set time/date for 5 day forecast
+    let forecastDates = moment().add(i + 1, 'days').format("MMMM Do, YYYY, hh:mm");
+  // Build HTML from js for 5-day forecast 
     let day = document.createElement("div");
     day.innerHTML = [
-      `<p>Temperature: ${data.daily[i].temp} F</p>`,
+      `<h5>Date: ${forecastDates}</h5>`,
+      `<img src="https://openweathermap.org/img/wn/${data.daily[i].weather[0].icon}@2x.png">`,
+      `<p>${data.daily[i].weather[0].description}</p>`,
+      `<p></p>`,
+      `<p>Temperature: ${data.daily[i].temp.day}°F</p>`,
       `<p>Humidity: ${data.daily[i].humidity}%</p>`,
       `<p>Wind Speed: ${data.daily[i].wind_speed}</p>`,
       `<p>UV Index: ${data.daily[i].uvi}</p>`,
     ];
     document.querySelector("#fiveDayContainers").appendChild(day);
   }
-
-  // document.querySelector("#currentCity").innerHTML = data.name;
-  // document.querySelector("#temp").innerHTML = data.main.temp;
-  // finish the rest of today for html
 }
 
-// Convert temp to C/F
-function altTemp() {
-  if (FC) {
-    temp = (temp - 32) * (5 / 9);
-    FC = false;
-    $(".temp").text("C");
-  } else {
-    temp = temp * (9 / 5) + 32;
-    FC = true;
-    $(".temp").text("F");
-  }
-}
+
+
+// Pull current UVI from second api data to get current uvI
+
+  // let uvi = data.current.uvi;
+  // document.querySelector("#uvIndex").innerHTML = uvi;
+  // document.querySelector("#currentWeather").append(uvi);
