926f05e2b29daae8c21482e1933c74552ac19e83
Niki Catapano <nsc9605@gmail.com>
2021-01-10 17:35:10 -0500

    added more styling and button for previous searches

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,8 @@
 console.log("works");
-
+// window.onload = function() {
 // Global Variables
 let apiKey = "c6c3415f70d6fade0d7d3230231ef65e";
-let currentDate = moment().format("MM/DD/YYYY");
+// let currentDate = moment().format("MM/DD/YYYY");
 let lat = "";
 let lon = "";
 let currentCity = "#currentCity";
@@ -34,11 +34,11 @@ function renderSaveBtns() {
     searchHistoryBtn.addClass("btn btn-secondary searchHistoryBtn");
     searchHistoryBtn.text(citySearches);
     $("searchHistory").prepend(searchHistoryBtn);
-    console.log(searchHistoryBtn);
+    // console.log(searchHistoryBtn);
   })
 }
 
-
+renderSaveBtns();
 // Submit event
 document
   .querySelector(".search-form")
@@ -50,13 +50,14 @@ document
     getWeather(cityInput);
     // saveToStorage(newCitySearch);
     saveToStorage(cityInput);
-    renderSaveBtns(citySearches);
+    renderSaveBtns();
 
   });
 
 $(".searchHistoryBtn").click(function () {
   var searchHistoryCity = $(this).text();
   getWeather(searchHistoryCity);
+  renderSaveBtns();
 })
 
 // API call to get lat and lon coordinates
@@ -85,46 +86,83 @@ function getWeather(cityName) {
     });
 }
 
+renderSaveBtns();
 // Show data on page
 function showWeatherForToday(cityName, data) {
- 
-  document.querySelector("#currentDate").innerHTML = moment().format("MMMM Do, YYYY, hh:mm");
+//  let currentDate = moment().format("MMMM Do, YYYY");
+  // let currentDay = document.createElement("div");
+  //   currentDay.innerHTML = [
+  //     `<h4>${currentDate}</h4>
+  //     <h5>City: ${cityName}</h5>span></h5>
+  //     <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png">
+  //     <p>${data.weather[0].description}</p>
+  //     <p>Temperature: ${data.main[0].temp}°F</p>
+  //     <p>High Temp: ${data.main[0].temp_max}°F</p>
+  //     <p>Low Temp: ${data.main[0].temp_min}°F</p>
+  //     <p>Humidity: ${data.main[0].humidity}%</p>
+  //     <p>Wind Speed: ${data.wind[0].wind_speed}</p>
+  //     <p>UV Index: ${data.current[0].uvi}</p>`,
+  //   ];
+  //   document.querySelector("#currentWeather").appendChild(currentDay);
+
+  document.querySelector("#currentDate").innerHTML = moment().format("MMMM Do, YYYY");
   document.querySelector("#weatherDescription").innerHTML = data.weather[0].description;
-  document.querySelector("#currentIcon").innerHTML = `<img src="http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png"`;
-  // document.querySelector("#cityImage").innerHTML = `<img src="http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+  document.querySelector("#currentIcon").innerHTML = `<img src="http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png"/>`;
   document.querySelector("#currentCity").innerHTML = cityName;
-  document.querySelector("#temp").innerHTML = data.main.temp + "°F";
+  document.querySelector("#temp").innerHTML = Math.round(data.main.temp) + "°F";
   document.querySelector("#humidity").innerHTML = data.main.humidity + "%";
-  document.querySelector("#wind").innerHTML = data.wind.speed + "mph";
-  // document.querySelector("#uvIndex").innerHTML = data.main.uvi;
+  document.querySelector("#wind").innerHTML = Math.round(data.wind.speed) + "mph";
+
+  // $("#card-text").empty();
+  // $("#card-text").append(currentWeather);
+  
  }
 
 // five day
 function showFiveDayWeather(data) {
 
+  // Grabs UVI info from 5-day forecast API 
+  // let currentIcon = document.querySelector("#currentIcon").innerHTML = `<img src="http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png"`
+  let currentUVI = document.querySelector("#currentUVI").innerHTML = Math.round(data.current.uvi);
+  document.querySelector("#currentWeather").append(currentUVI);
+  
+  // SET BADGES FOR UVI HIGH / LOW
+
+      // if (currentUVI <= 2) {
+        //   currentUVI.addClass("badge badge-success");
+        //   } else if (currentUVI > 2 && currentUVI <= 5) {
+        //     currentUVI.addClass("badge badge-warning");
+        //   } else if (currentUVI > 5) {
+        //     currentUVI.addClass("badge badge-danger");
+        //   };
+      // currentUVI.innerHTML = `UV Index: ${data.current.uvi}`;
+
+
+
   // For loop to pull weather for 5 day forecast
   for (var i = 0; i < 5; i++) {
+
     // Set time/date for 5 day forecast
-    let forecastDates = moment().add(i + 1, 'days').format("MMMM Do, YYYY, hh:mm");
+    let forecastDates = moment().add(i + 1, 'days').format("ddd MM/DD/YYYY");
+
   // Build HTML from js for 5-day forecast 
     let day = document.createElement("div");
     day.innerHTML = [
-      `<h5>Date: ${forecastDates}</h5>`,
-      `<img src="https://openweathermap.org/img/wn/${data.daily[i].weather[0].icon}@2x.png">`,
-      `<p>${data.daily[i].weather[0].description}</p>`,
-      `<p></p>`,
-      `<p>Temperature: ${data.daily[i].temp.day}°F</p>`,
-      `<p>Humidity: ${data.daily[i].humidity}%</p>`,
-      `<p>Wind Speed: ${data.daily[i].wind_speed}</p>`,
-      `<p>UV Index: ${data.daily[i].uvi}</p>`,
+      `<h5>${forecastDates}</h5>
+      <img src="https://openweathermap.org/img/wn/${data.daily[i].weather[0].icon}@2x.png" style="center">
+      <p>${data.daily[i].weather[0].description}</p>
+      <p>Temperature: ${Math.round(data.daily[i].temp.day)}°F</p>
+      <p>High Temp: ${Math.round(data.daily[i].temp.max)}°F</p>
+      <p>Low Temp: ${Math.round(data.daily[i].temp.min)}°F</p>
+      <p>Humidity: ${data.daily[i].humidity}%</p>
+      <p>Wind Speed: ${Math.round(data.daily[i].wind_speed)}</p>
+      <p>UV Index: ${data.daily[i].uvi}</p>
+      <br>`,
     ];
     document.querySelector("#fiveDayContainers").appendChild(day);
+
+    renderSaveBtns();
   }
 }
 
-
-// Pull current UVI from second api data to get current uvI
-
-  // let uvi = data.current.uvi;
-  // document.querySelector("#uvIndex").innerHTML = uvi;
-  // document.querySelector("#currentWeather").append(uvi);
+// };
